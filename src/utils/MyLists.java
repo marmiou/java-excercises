31ca6457fe861367e6bb3c6d70e7b34e20913c83
Markella Efthymiou <efthymioumarkella@gmail.com>
2023-03-12 17:14:42 +0200

    Update MyLists.java

--- src/utils/MyLists.java
+++ src/utils/MyLists.java
@@ -1,7 +1,9 @@
 package utils;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -18,9 +20,8 @@ public class MyLists {
     }
 
     public List<Integer> sortList(List<Integer> myList) {
-        List<Integer> sortedList = myList;
-        Collections.sort(sortedList);
-        return sortedList;
+        Collections.sort(myList);
+        return myList;
     }
 
     public int getMinNumber(List<Integer> myList) {
@@ -36,14 +37,38 @@ public class MyLists {
     public int countStringsWithLastFirstChar(List<String> myList) {
         AtomicInteger total = new AtomicInteger();
         myList.forEach(str -> {
-            if (str.charAt(0) == str.charAt(str.length() - 1) && str.length()>1) {
+            if (str.charAt(0) == str.charAt(str.length() - 1) && str.length() > 1) {
                 total.getAndIncrement();
             }
         });
         return total.get();
     }
 
-    public List<Integer> removeDuplicates(List<Integer> myList){
+    public List<Integer> removeDuplicates(List<Integer> myList) {
         return myList.stream().distinct().collect(Collectors.toList());
+    }
+
+    public Boolean checkIsEmpty(List<Integer> myList) {
+        if (myList.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    public List<Integer> checkAtLeastOneCommonMember(List<Integer> listOne, List<Integer> listTwo) {
+       return listOne.stream().filter(itemOne -> listTwo.stream().anyMatch(itemTwo -> itemTwo.equals(itemOne)))
+                .collect(Collectors.toList());
+    }
+
+//    public List<Integer> getDifference(List<Integer> listOne, List<Integer> listTwo){
+//        return listOne.stream().filter(itemOne -> !listTwo.contains(itemOne))
+//                .collect(Collectors.toList());
+//    }
+
+    public Map<Integer,Integer> accessIndexAndValue(List<Integer> myList){
+        AtomicInteger index = new AtomicInteger();
+        Map<Integer, Integer>  indexAndValue = new HashMap<>();
+        myList.forEach(item -> indexAndValue.put(index.getAndIncrement(),item));
+        return indexAndValue;
     }
 }
