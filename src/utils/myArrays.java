d6913498a459c7227eedcc2b5d718f857899af5e
Markella Efthymiou <efthymioumarkella@gmail.com>
2023-03-13 18:40:51 +0200

    Update myArrays.java

--- src/utils/myArrays.java
+++ src/utils/myArrays.java
@@ -25,11 +25,39 @@ public class MyArrays {
             }
         });
         return max[0];
-
     }
 
     public Boolean checkOccurence(Character[] charArray, char character) {
         return Arrays.stream(charArray)
                 .anyMatch(item -> item == character);
+    }
+
+    public int[] reverse(int[] intArray) {
+        //1) Initialize start and end indexes as start = 0, end = n-1
+        //2) In a loop, swap arr[start] with arr[end] and change start and end as follows :
+        //start = start +1, end = end – 1
+        int start = 0;
+        int end = intArray.length - 1;
+        while (start < end) {
+            int temp = intArray[start];
+            intArray[start] = intArray[end];
+            intArray[end] = temp;
+            start++;
+            end--;
+        }
+        return intArray;
+    }
+
+    public char[] reverse(char[] charArray) {
+        int start = 0;
+        int end = charArray.length - 1;
+        while (start < end) {
+            char temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+            start++;
+            end--;
+        }
+        return charArray;
     }
 }
